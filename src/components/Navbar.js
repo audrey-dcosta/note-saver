e9d29ed6ce5ca027002def79c0d3ea815089ff91
audrey-dcosta <audrey2795@gmail.com>
2021-09-21 21:18:36 +0530

    created base components

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import NotebookList from "./NotebookList";
+import NoteList from "./NoteList";
+import Profile from "./Profile";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+  } from "react-router-dom";
+  
+
+export default function Navbar() {
+    return (
+        <div>
+            <Router>
+    <div>
+      <ul className="flex justify-center border-b-2 border-gray-300 shadow-inner bg-white">
+        <li className="p-4 px-6 text-xl hover:shadow-inner hover:font-bold hover:text-orange-500 ">
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li  className="p-4 px-6 text-xl hover:shadow-inner hover:font-bold hover:text-orange-500">
+          <Link to="/notebooks">Notebooks</Link>
+        </li>
+        <li  className="p-4 px-6 text-xl hover:shadow-inner hover:font-bold hover:text-orange-500">
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/">
+          <NotebookList />
+        </Route>
+        <Route path="/notes">
+          <NotebookList/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+        <Route path="/notebook/:id">
+          <NoteList />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+            
+        </div>
+    )
+}
