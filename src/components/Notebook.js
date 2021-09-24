fd09fc80e882db06bc55c5eb8d7200d5bf227310
audrey-dcosta <audrey2795@gmail.com>
2021-09-24 18:03:42 +0530

    notebook style change

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -4,20 +4,38 @@ import {ImFilesEmpty}  from "react-icons/im";
 
 export const Notebook = ({notebook,handleClick}) => {
     return (
-        <div onClick={()=>{handleClick(notebook.notebook_id)}} className=" group flex flex-col justify-between bg-white rounded-lg p-4 pb-2 shadow-xl border-4 border-white hover:bg-orange-550 hover:border-orange-550 ">
-        <div className="">
+        <div onClick={()=>{handleClick(notebook.notebook_id)}} className="box-content bg-white w-full rounded-lg">
+        <div className="p-2 sm:p-4 flex flex-col justify-between h-full">
+            <div className="w-full">
             <div className="text-lg sm:text-2xl flex space-x-2 text-gray-900 font-bold items-center group-hover:text-gray-50"><AiFillBook/>
-                <span>{notebook.notebook_name}</span>
-            </div>
+                 <span>{notebook.notebook_name}</span>
+             </div>
             <div className="text-sm sm:text-md py-2 text-gray-500 group-hover:text-gray-50">{notebook.notebook_desc}</div>
-        </div>
-        <div className="flex justify-end ">
-            <div className="flex align-middle space-x-1 text-gray-500 font-semibold p-1 mt-2 text-sm group-hover:text-gray-50">
-                <ImFilesEmpty/>
-                <span>12</span>
+                
             </div>
+        <div className="mt-2 sm:mt-10 w-1/5 place-self-end ">
+        <div className="flex align-middle space-x-1 text-gray-500 font-semibold p-1 mt-2 text-sm group-hover:text-gray-50">
+                 <ImFilesEmpty/>
+             <span>12</span>
+             </div>
+
         </div>
-    </div>
+        </div>
+</div>
+    //     <div onClick={()=>{handleClick(notebook.notebook_id)}} className=" group flex flex-col justify-between bg-white rounded-lg p-4 pb-2 shadow-xl border-4 border-white hover:bg-orange-550 hover:border-orange-550 ">
+    //     <div className="">
+    //         <div className="text-lg sm:text-2xl flex space-x-2 text-gray-900 font-bold items-center group-hover:text-gray-50"><AiFillBook/>
+    //             <span>{notebook.notebook_name}</span>
+    //         </div>
+    //         <div className="text-sm sm:text-md py-2 text-gray-500 group-hover:text-gray-50">{notebook.notebook_desc}</div>
+    //     </div>
+    //     <div className="flex justify-end ">
+    //         <div className="flex align-middle space-x-1 text-gray-500 font-semibold p-1 mt-2 text-sm group-hover:text-gray-50">
+    //             <ImFilesEmpty/>
+    //             <span>12</span>
+    //         </div>
+    //     </div>
+    // </div>
 
     )
 }
