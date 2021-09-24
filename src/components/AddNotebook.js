17a55ad23c0cdd8e1f42b6b8d091e6eb52d4b59f
audrey-dcosta <audrey2795@gmail.com>
2021-09-24 18:33:57 +0530

    created add notebook component

---
+++ src/components/AddNotebook.js
@@ -0,0 +1,105 @@
+import React,{ Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+export const AddNotebook = ({isOpen,closeModal}) => {
+    const initialFormData=Object.freeze({notebook_name:'',notebook_description:''})
+    const [formData,updateFormData]=useState(initialFormData)
+    
+    const handleChange=(e)=>{
+      e.preventDefault()
+      updateFormData({
+        ...formData,
+        [e.target.name]:e.target.value.trim()
+      })
+      console.log('bbbbb')
+      // setNotebook(e.target.name:e.target.value)
+  }
+  
+  const handleSubmit=(e)=>{
+    console.log('aaaaa')
+    e.preventDefault()
+    updateFormData({
+      ...formData,
+    })
+    console.log(formData)
+  }
+    return (
+        <>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-10 overflow-y-auto"
+            onClose={closeModal}
+          >
+            <div className="min-h-screen px-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-30" />
+              </Transition.Child>
+  
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span
+                className="inline-block h-screen align-middle"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-2xl font-medium leading-6 text-gray-900"
+                  >
+                    Add New Notebook
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <form onSubmit={handleSubmit}>
+                      <input name="notebook_name" onChange={handleChange} className="p-2 my-4 ring-2 text-xl ring-gray-300 rounded-lg w-full focus:outline-none focus:ring-orange-550" type="text" placeholder="Enter Notebook name"/>
+                      <textarea name="notebook_description" onChange={handleChange} className="p-2 my-4 ring-2 text-xl ring-gray-300 rounded-lg w-full focus:outline-none focus:ring-orange-550" placeholder="Description"/>
+                      {/* <div className="flex space-x-4">
+                      <input className="p-2 my-4 ring-2 text-xl ring-gray-300 rounded-lg w-full focus:outline-none focus:ring-orange-550" type="text" placeholder="Add Tag"/>
+                      <button className="ring-2 bg-orange-550 ring-orange-550 my-4 p-1 text-gray-50 rounded-lg font-medium px-3 text-3xl">+</button>
+                      </div> */}
+                    </form>
+                  </div>
+  
+                  <div className="mt-4 flex justify-end space-x-4">
+                  <button
+                      type="Submit"
+                      className="inline-flex justify-center px-4 py-2 text-lg font-medium text-gray-900 bg-gray-100 border border-transparent rounded-md hover:bg-orange-550 hover:text-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center px-4 py-2 text-lg font-medium text-gray-900 bg-gray-100 border border-transparent rounded-md hover:bg-orange-550 hover:text-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      onClick={handleSubmit}
+                    >
+                      Add
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+      </>
+  
+    )
+}
--- src/components/NotebookList.js
+++ src/components/NotebookList.js
@@ -1,19 +1,25 @@
 import React,{useState,useEffect} from 'react'
 import { Notebook } from './Notebook'
+import { AddNotebook } from './AddNotebook'
 import {GoPlus} from 'react-icons/go'
-import {Redirect,useParams,useHistory,generatePath} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 import {SkeletonNotebook} from './Skeletons/SkeletonNotebook'
 
 
 export default function NotebookList() {
     const [notebooks, setNotebooks] = useState(null)
-    const [isRedirect, setIsRedirect] = useState(false)
-    const [id, setId] = useState()
+    let [isOpen,setIsOpen]= useState(false)
+    
     const history= useHistory()
 
     const handleClick=(id)=>{
         history.push(`/notebook/${id}`);
     }
+
+    function toggleModal() {
+        setIsOpen(!isOpen)
+      }
+
 
 
     useEffect(() => {
@@ -30,7 +36,7 @@ export default function NotebookList() {
         <div className="p-1 sm:p-6 flex items-center justify-center w-full ">
         {notebooks && (
             <div className="grid grid-cols-2 items-center justify-items-center gap-1 w-full sm:w-11/12 sm:grid-cols-4 sm:gap-4 md:w-4/6">
-                <div class="group text-3xl box-content  w-full rounded-lg text-orange-550 flex flex-col justify-center items-center border-4 h-full border-orange-550 hover:bg-orange-550 hover:shadow-lg hover:text-white hover:border-transparent">
+                <div onClick={toggleModal} class="group text-3xl box-content  w-full rounded-lg text-orange-550 flex flex-col justify-center items-center border-4 h-full border-orange-550 hover:bg-orange-550 hover:shadow-lg hover:text-white hover:border-transparent">
                 <GoPlus/>
                 </div>
                 {notebooks.map((notebook)=><Notebook key={notebook.notebook_id} notebook={notebook} handleClick={handleClick}>
@@ -45,7 +51,9 @@ export default function NotebookList() {
                     {[1,2,3,4,5,6,7,8].map((i)=><SkeletonNotebook key={i}/>)}
                 </div>
                 )}
-    {isRedirect?<Redirect to={`/notebook/:${id}`}/>:null}
+            
+            {isOpen && <AddNotebook isOpen={isOpen} closeModal={toggleModal}></AddNotebook>}
+
     </div>
     )
 }
